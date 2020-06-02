ced4737160aa78a6eb83b7ad58a72ccfb0257508
Timbogen <niederer.tim@gmail.com>
2020-06-02 20:30:50 +0200

    feat(Shared): Add input field

--- src/General/Shared/MBLabel.java
+++ src/General/Shared/MBLabel.java
@@ -6,6 +6,10 @@ import java.awt.*;
 public class MBLabel extends JLabel {
 
     /**
+     * Name of the global font
+     */
+    public static final String FONT_NAME = "Calibri";
+    /**
      * Fontsize for H1 text
      */
     public static final int H1 = 20;
@@ -69,6 +73,6 @@ public class MBLabel extends JLabel {
      * @param fontSize of the text
      */
     private void setupLabel(int fontSize) {
-        setFont(new Font("Calibri", Font.PLAIN, fontSize));
+        setFont(new Font(FONT_NAME, Font.PLAIN, fontSize));
     }
 }
