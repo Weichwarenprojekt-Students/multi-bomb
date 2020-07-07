67e2c18d3185722bb31830a3f3ac1d956540bf84
jsx15 <>
2020-07-07 08:27:23 +0200

    style(ServerList.java): Update comments and updateListView()

--- src/Server/ServerList.java
+++ src/Server/ServerList.java
@@ -10,40 +10,52 @@ public class ServerList {
     /**
      * List for local found servers
      */
-    ArrayList<ServerView.ServerListItem> localServerList = new ArrayList<>();
+    private ArrayList<ServerView.ServerListItem> localServerList = new ArrayList<>();
     /**
      * List for remote added servers
      */
-    public ArrayList<ServerView.ServerListItem> remoteServerList = new ArrayList<>();
+    private ArrayList<ServerView.ServerListItem> remoteServerList = new ArrayList<>();
     /**
      * List that contains all servers
      */
-    public ArrayList<ServerView.ServerListItem> allServerList = new ArrayList<>();
+    private ArrayList<ServerView.ServerListItem> allServerList = new ArrayList<>();
     /**
-     *
+     * Detectserver
      */
-    DetectServer ds = new DetectServer();
+    private DetectServer ds = new DetectServer();
 
     public ServerList() {
         MB.settings.remoteServer.forEach(server -> new Thread(new ScanServerThread(server, remoteServerList, "remote")).start());
     }
 
+    /**
+     * Add all found servers to allServerList
+     */
     public void updateAllServerList () {
         allServerList.clear();
         allServerList.addAll(localServerList);
         allServerList.addAll(remoteServerList);
     }
 
+    /**
+     * Search for servers in local network
+     */
     public void searchLocalServer () {
         localServerList.clear();
         ds.search(localServerList);
     }
 
+    /**
+     * Update ListView
+     */
     public void updateListView (MBListView<ServerView.ServerListItem> listView) {
-        listView.removeAllItems();
-        allServerList.forEach(listView::addItem);
+        listView.addMissingItems(allServerList);
     }
 
+    /**
+     * Add server address to settings
+     * Scan remote server
+     */
     public void addRemoteServer (String serverAddress) {
         MB.settings.remoteServer.add(serverAddress);
         MB.settings.saveSettings();
